de51fc5215b41fc106d4497ed5bb16d69c212bea
Patiljagadish1996 <107257868+Patiljagadish1996@users.noreply.github.com>
2023-05-24 23:27:54 +0530

    Add files via upload

---
+++ portfolio/myportfolio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'myportfolio/home.html')
+
+def about(request):
+	return render(request, 'myportfolio/about.html')
+
+def portfolio(request):
+	return render(request, 'myportfolio/portfolio.html')
+
+def contact(request):
+	return render(request, 'myportfolio/contact.html')
